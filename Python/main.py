661f5f3770f7c41eeef596f272b535c19072199f
a2smirno <87276007+a2smirno@users.noreply.github.com>
2021-07-11 14:36:04 -0400

    Add files via upload

--- Python/main.py
+++ Python/main.py
@@ -59,8 +59,6 @@ projections = tigre.Ax(shepp, geo, angles)
 # noise_projections = CTnoise.add(projections, Poisson=1e5, Gaussian=np.array([0, 10]))
 noise_projections = projections
 
-#%% recosntruct
-
 #%% Total Variation algorithms
 #
 #  ASD-POCS: Adaptative Steeppest Descent-Projection On Convex Subsets
@@ -125,44 +123,6 @@ verb = True
 
 niter = 10
 
-imgOSSART = algs.ossart(projections, geo, angles, niter)
-imgASDPOCS = algs.asd_pocs(
-    projections,
-    geo,
-    angles,
-    10,  # these are very important
-    tviter=ng,
-    maxl2err=epsilon,
-    alpha=alpha,  # less important.
-    lmbda=lmbda,
-    lmbda_red=lambdared,
-    rmax=ratio,
-    verbose=verb,
-)
-
-#  AwASD_POCS: adaptative weighted ASD_POCS
-# ==========================================================================
-# ==========================================================================
-#
-# This is a more edge preserving algorithms than ASD_POCS, in theory.
-# delta is the cuttof vlaue of anromalized edge exponential weight....
-# not super clear, but it cotnrols at which point you accept something as real vs noise edge.
-
-imgAWASDPOCS = algs.awasd_pocs(
-    projections,
-    geo,
-    angles,
-    10,  # these are very important
-    tviter=ng,
-    maxl2err=epsilon,
-    alpha=alpha,  # less important.
-    lmbda=lmbda,
-    lmbda_red=lambdared,
-    rmax=ratio,
-    verbose=verb,  # AwASD_POCS params
-    delta=np.array([-0.005]),
-)
-
 imgDTVASDPOCS = algs.dtv_asd_pocs(
     projections,
     geo,
@@ -180,24 +140,12 @@ imgDTVASDPOCS = algs.dtv_asd_pocs(
 
 # Measure Quality
 # 'RMSE', 'MSSIM', 'SSD', 'UQI'
-print("RMSE OSSART:")
-print(Measure_Quality(imgOSSART, shepp, ["nRMSE"]))
-print("RMSE ASDPOCS:")
-print(Measure_Quality(imgASDPOCS, shepp, ["nRMSE"]))
-print("RMSE AWASDPOCS:")
-print(Measure_Quality(imgAWASDPOCS, shepp, ["nRMSE"]))
 print("RMSE DTVASDPOCS:")
 print(Measure_Quality(imgDTVASDPOCS, shepp, ["nRMSE"]))
 
 #%% Plot
 plt.imsave('shepp-logan.png', shepp[0])
-plt.imsave('os-sart.png', imgOSSART[0])
-plt.imsave('asd-pocs.png', imgASDPOCS[0])
-plt.imsave('aw-asd-pocs.png', imgAWASDPOCS[0])
 plt.imsave('dtv-asd-pocs.png', imgDTVASDPOCS[0])
-plt.imsave('os-sart-err.png', imgOSSART[0]-shepp[0])
-plt.imsave('asd-pocs-err.png', imgASDPOCS[0]-shepp[0])
-plt.imsave('aw-asd-pocs-err.png', imgAWASDPOCS[0]-shepp[0])
 plt.imsave('dtv-asd-pocs-err.png', imgDTVASDPOCS[0]-shepp[0])
 
 # tigre.plotProj(proj)
